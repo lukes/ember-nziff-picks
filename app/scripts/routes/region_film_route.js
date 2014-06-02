a96220b0d20d03394509717f16a3b24691b673f3
Luke Duncalfe <lduncalfe@eml.cc>
2014-06-02 21:47:24 +1200

    Importing more Films and Reviews.

--- app/scripts/routes/region_film_route.js
+++ app/scripts/routes/region_film_route.js
@@ -1,5 +1,6 @@
 NziffPicks.RegionFilmRoute = Ember.Route.extend({
   model: function (params) {
+    console.log(this.store.findById('film', params.film_id).get('reviews'));
     return this.store.findById('film', params.film_id);
     // TODO check and handle that this film gets screened in this region
   }
